22972d8692852ea27b66288720a3bdf54766771a
Christine <pt2534@columbia.com>
2020-03-27 17:08:20 +0000

    [Big_Data] Search news posts by quarter
    1. Can specify the duration
    2. Need to fix some errors in obtaining posts

--- news_crawler.py
+++ news_crawler.py
@@ -12,32 +12,68 @@ import json
 import os
 import csv
 import sys
+import re
+import urllib3
 
 # In some website, the article is not in <p>, so will be empty. Check articles 
 # dictionary to delete empty news
 
 skip_list = ['電腦及周邊設備', '上游', '下游']
 
-def search(key_word="null"):
+search_period = {
+        "Q1": "min%3A1%2F1%2F2019%2Ccd_max%3A3%2F31%2F2019&tbm=nws",
+        "Q2": "min%3A4%2F1%2F2019%2Ccd_max%3A6%2F30%2F2019&tbm=nws",
+        "Q3": "min%3A7%2F1%2F2019%2Ccd_max%3A9%2F30%2F2019&tbm=nws",
+        "Q4": "min%3A10%2F1%2F2019%2Ccd_max%3A9%2F31%2F2019&tbm=nws"
+}
+
+def search(key_word="null", period=None):
     articles = {}
 
-    print('Key_word: {0}'.format(key_word))
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
     if key_word == "null" or key_word in skip_list:
-        print("The key word is empty or in the skip list")
+        #print("The key word is empty or in the skip list")
         return articles
     
     keyword = quote(key_word.encode('utf8'))
+   
+    #print('keyword: {0}'.format(keyword))
+    #res = requests.get("https://news.google.com/search?q=" + keyword + "when%3A4d&hl=zh-TW&gl=TW&ceid=TW%3Azh-Hant") #when%3A幾d 爬幾天之內新聞
+    # Sample request in Google search 
+    # https://www.google.com/search?q=%E5%8F%B0%E7%A9%8D%E9%9B%BB&client=safari&rls=en&biw=1780&bih=946&source=lnt&tbs=cdr%3A1%2Ccd_min%3A7%2F1%2F2019%2Ccd_max%3A9%2F30%2F2019&tbm=nws
+    res = requests.get("https://www.google.com/search?q=" + keyword + "&client=safari&rls=en&biw=1780&bih=946&source=lnt&tbs=cdr%3A1%2Ccd_" + period, verify=False)
     
-    res = requests.get("https://news.google.com/search?q=" + keyword + "when%3A4d&hl=zh-TW&gl=TW&ceid=TW%3Azh-Hant") #when%3A幾d 爬幾天之內新聞
     if res.status_code != 200:
         return articles
+
+    #soup = BeautifulSoup(res.content, "html.parser")
+    #headline = soup.findAll("a", {"class": "DY5T1d"})
     
-    soup = BeautifulSoup(res.content, "html.parser")
-    headline = soup.findAll("a", {"class": "DY5T1d"})
-    
-    for h in headline:
+    headline = []
+    soup = BeautifulSoup(res.text, 'lxml')
+    a = soup.find_all('a') 
+    for i in a:
+        k = i.get('href')
         try:
-            res1 = requests.get('https://news.google.com' + h['href'][1:], timeout=30)
+            m = re.search("(?P<url>https?://[^\s]+)", k)
+            n = m.group(0)
+            rul = n.split('&')[0]
+            domain = urlparse(rul)
+            if(re.search('google.com', domain.netloc)):
+                continue
+            else:
+                headline.append(rul)
+        except:
+            continue
+
+    #print('total number of the news: {0}'.format(len(headline)))
+
+    # Only iterate 100 posts
+    for h in headline[0:100]:
+        #print(h) 
+        
+        try:
+            res1 = requests.get(h)
         except requests.ConnectionError as e:
             print("OOPS!! Connection Error. Make sure you are connected to Internet. Technical Details given below.\n")
             print(str(e))
@@ -62,13 +98,13 @@ def search(key_word="null"):
         cont = ''
         for l in lines:
             cont += l.text
-        articles[h.text] = cont
-
+        articles[h] = cont
+        #print(cont)
 
     return articles
 
 
-def parse_csv(file_name = "None", dest_path = "./"):
+def parse_csv(file_name = "None", dest_path = "./", period=None):
     with open(file_name) as csvfile:
         readCSV = csv.reader(csvfile, delimiter=',')
         for row in readCSV:
@@ -76,16 +112,16 @@ def parse_csv(file_name = "None", dest_path = "./"):
             if len(row) == 0:
                 break
             if len(row) > 1:
-                msg0 = search(row[0])
+                msg0 = search(row[0], period)
                 if msg0 != None:
                     save_data(msg0, row[0], dest_path)
                 
                 if sys.argv[1] != "Relationship.csv":
-                    msg1 = search(row[1])
+                    msg1 = search(row[1], period)
                     if msg1 != None:
                         save_data(msg1, row[1], dest_path)
            
-                    msg2 = search(row[2])
+                    msg2 = search(row[2], period)
                     if msg2 != None:
                         save_data(msg2, row[2], dest_path)
 
@@ -106,7 +142,10 @@ def save_data(articles, file_name, path):
 if __name__== "__main__":
     print("This is news crawler")
     print('file = {0}, path = {1}'.format(sys.argv[1], sys.argv[2]))
-    data = parse_csv(sys.argv[1], sys.argv[2])
+    
+    #urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    for QQQ in search_period:
+        data = parse_csv(sys.argv[1], sys.argv[2], QQQ)
 
 
 
