9f000ef6542fc26156580d2d8dcf87f57158825e
waynelaizye <32830753+waynelaizye@users.noreply.github.com>
2020-05-02 01:27:15 +0800

    Update news_crawler_v2.py

--- src/news_crawler_v2.py
+++ src/news_crawler_v2.py
@@ -16,24 +16,33 @@ import sys
 import re
 import urllib3
 import time
+import pathlib
+
+RAW_DATA_PATH = os.path.join(pathlib.Path(__file__).parent.parent, 'data/raw')
 
 # In some website, the article is not in <p>, so will be empty. Check articles 
 # dictionary to delete empty news
-skip_list = ['電腦及周邊設備', '上游', '下游']
-input_file = 'test.txt'
-save_dir = 'president_news'
-is_list = False
+file_name = 'company.txt'
+#file_name = os.path.join(RAW_DATA_PATH, 'product.txt')
+save_path = 'company_news'
+is_list = True
 
 
 search_period = {
         "Q1": "min%3A1%2F1%2F2019%2Ccd_max%3A3%2F31%2F2019&tbm=nws",
         "Q2": "min%3A4%2F1%2F2019%2Ccd_max%3A6%2F30%2F2019&tbm=nws",
         "Q3": "min%3A7%2F1%2F2019%2Ccd_max%3A9%2F30%2F2019&tbm=nws",
-        "Q4": "min%3A10%2F1%2F2019%2Ccd_max%3A9%2F31%2F2019&tbm=nws",
+        "Q4": "min%3A10%2F1%2F2019%2Ccd_max%3A12%2F31%2F2019&tbm=nws",
         "Q5": "min%3A1%2F1%2F2020%2Ccd_max%3A3%2F31%2F2020&tbm=nws"
 }
 
-def read_list(file_name=input_file):
+def is_crawled(name):
+    files = os.listdir(save_path)
+    if name+'.json' in files:
+        return True
+    return False
+
+def read_list():
     print('Reading', file_name)
     word_list = []
     with open(file_name, encoding = 'utf8') as f:
@@ -43,7 +52,7 @@ def read_list(file_name=input_file):
     print('Total', len(word_list), 'lines')
     return word_list
 
-def read_json(file_name=input_file):
+def read_json():
     print('Reading', file_name)
     with open(file_name, encoding = 'utf8') as f:
         data = json.load(f)
@@ -53,45 +62,38 @@ def search(key_word="null", period=""):
     articles = {}
 
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-    if key_word == "null" or key_word in skip_list:
-        #print("The key word is empty or in the skip list")
-        return None
     
     keyword = quote(key_word.encode('utf8'))
     print('duration = {0}'.format(search_period[period]))
    
     # Sample request in Google search 
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}# Sample request in Google search 
     # https://www.google.com/search?q=%E5%8F%B0%E7%A9%8D%E9%9B%BB&client=safari&rls=en&biw=1780&bih=946&source=lnt&tbs=cdr%3A1%2Ccd_min%3A7%2F1%2F2019%2Ccd_max%3A9%2F30%2F2019&tbm=nws
-    res = requests.get("https://www.google.com/search?q=" + keyword + "&client=safari&rls=en&biw=1780&bih=946&source=lnt&tbs=cdr%3A1%2Ccd_" + search_period[period], verify=False)
+    res = requests.get("https://www.google.com/search?q=" + keyword + "&client=safari&rls=en&biw=1780&bih=946&source=lnt&tbs=cdr%3A1%2Ccd_" + search_period[period], headers = headers, verify=False)
     
     if res.status_code != 200:
         return articles
 
-    headline = []
+    url = []
     soup = BeautifulSoup(res.text, 'lxml')
     a = soup.find_all('a') 
     for i in a:
         k = i.get('href')
         #print(k)
         try:
-            m = re.search("(?P<url>https?://[^\s]+)", k)
-            n = m.group(0)
-            rul = n.split('&')[0]
-            domain = urlparse(rul)
-            if(re.search('google.com', domain.netloc)) or (re.search('zh.wikipedia.org', domain.netloc)):
-                continue
-            else:
-                headline.append(rul)
+            if k[:4] == 'http' and 'google' not in k:
+                url.append(k)
         except:
             continue
 
     #print('total number of the news: {0}'.format(len(headline)))
-
+    url = list(set(url))
     # Only iterate 100 posts
-    for h in headline[0:100]:
+    for h in url:
+        if len(articles) == 20:
+            break
         try:
-
-            resp = requests.get(h,  allow_redirects=False)
+            resp = requests.get(h, allow_redirects=False)
             soup = BeautifulSoup(resp.content, 'html.parser')
 #            test = soup.findAll(text = re.compile(key_word))
 #            
@@ -117,17 +119,21 @@ def search(key_word="null", period=""):
 #            print("Someone closed the program")
 
         # Concatenate all news together
-        soup1 = BeautifulSoup(resp.content, "html.parser")
-        lines = soup1.findAll('p')
-        cont = ''
-        for l in lines:
-            cont += l.text
-        articles[h] = cont
+        try:
+            lines = soup.findAll('p')
+            cont = ''
+            for l in lines:
+                cont += l.text
+            head = soup.find('title')
+            head = head.text.split('｜')[0].split('|')[0].split('-')[0]
+            articles[head] = cont
+        except:
+            continue
 #        print(cont)
 
     return articles
 
-def save_data(articles, word, save_path=save_dir):
+def save_data(articles, word, save_path):
     word = word.replace(" ", "_")
     word = word.replace("/", "")
     print('file name = {0}'.format(word))
@@ -142,30 +148,27 @@ def save_data(articles, word, save_path=save_dir):
 if __name__== "__main__":
 #    word_list = read_list()
     print("This is news crawler")
-    print('file = {0}, json_file = {1}, save_path = {2}'.format(sys.argv[0], sys.argv[1], sys.argv[2]))
-    
+    print('file = {0}, path = {1}'.format(file_name, save_path))
     if is_list:
-        word_list = read_list(sys.argv[1])
+        word_list = read_list()
         for words in word_list:
-            articles = []
             name = words[0]
-            for word in words:
-                articles += search(word)
-            save_data(articles, name, sys.argv[2])
-    else:
-        word_dic = read_json(sys.argv[1])
-        for name in word_dic:
-            p = "Q5"
-            articles = {}
-            for word in word_dic[name]:
-                articles.update(search(word, p))
-            save_data(articles, name+'_'+p, sys.argv[2])
-                
-            """
+            if is_crawled(name):
+                print(name, 'crawled, skipped.')
+                continue
+            articles = {"Q1":{}, "Q2":{}, "Q3":{}, "Q4":{}, "Q5":{}}
             for p in search_period:
-                articles = {}
+                for word in words:
+                    articles[p].update(search(word, p))
+            save_data(articles, name, save_path)
+    else:
+        word_dic = read_json()
+        for name in word_dic:
+            if is_crawled(name):
+                print(name, 'crawled, skipped.')
+                continue
+            articles = {"Q1":{}, "Q2":{}, "Q3":{}, "Q4":{}, "Q5":{}}
+            for p in search_period:
                 for word in word_dic[name]:
-                    articles.update(search(word, p))
-                save_data(articles, name+'_'+p, sys.argv[2])
-                time.sleep(1)
-            """
+                    articles[p].update(search(word, p))
+            save_data(articles, name, save_path)
